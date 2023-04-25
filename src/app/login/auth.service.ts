afe28fdbee2b9dc7365e0c42980af3e7c7fdb09d
Fernanda Winter <fernanda.winter@hotmail.com>
2023-04-25 15:57:47 -0300

    feat: creating auth.service file for login

---
+++ src/app/login/auth.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+     
+import {  Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+  
+import { Login } from './login';
+  
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  
+  private apiURL = "https://3kniis.sse.codesandbox.io";
+    
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      'accept': '*/*'
+    })
+  }
+  
+  constructor(private httpClient: HttpClient) { }
+    
+  authenticate(login:Login): Observable<any> {
+  
+    return this.httpClient.post(this.apiURL + '/auth/login', JSON.stringify(login), this.httpOptions)
+  
+    .pipe(
+      catchError(this.errorHandler)
+    )
+  }  
+    
+  errorHandler(error:any) {
+    let errorMessage = '';
+    if(error.error instanceof ErrorEvent) {
+      errorMessage = error.error.message;
+    } else {
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    return throwError(errorMessage);
+ }
+}
