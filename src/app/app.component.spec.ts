2c519075b91e85344f5a2a27ea986b5fcd9af2b1
Fernanda Winter <fernanda.winter@hotmail.com>
2023-04-28 18:08:04 -0300

    refactor: refactoring app component test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'frontend-challenge-picpay'`, () => {
+  it(`should have as title 'Sistema de Pagamentos'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('frontend-challenge-picpay');
+    expect(app.title).toEqual('Sistema de Pagamentos');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('frontend-challenge-picpay app is running!');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Sistema de Pagamentos');
   });
 });
