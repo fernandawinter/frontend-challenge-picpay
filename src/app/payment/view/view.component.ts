d4b4697a38e5ce98d268c670f88eac20866a5209
Fernanda Winter <fernanda.winter@hotmail.com>
2023-04-28 17:28:58 -0300

    refactor: deleting css file in view component

--- src/app/payment/view/view.component.ts
+++ src/app/payment/view/view.component.ts
@@ -6,7 +6,6 @@ import { Payment } from '../payment';
 @Component({
   selector: 'app-view',
   templateUrl: './view.component.html',
-  styleUrls: ['./view.component.css']
 })
 export class ViewComponent implements OnInit {
 
